4be288730f2c7394c8689eed91f46e137ff57567
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2022-06-23 19:43:04 +0900

    check

--- README.md
+++ README.md
@@ -6,30 +6,46 @@ Utility tool to ping and get the info of a running Starblast game lobby.
 
 ```bash
 npm install starblast-pinger
+yarn add starblast-pinger
+pnpm add starblast-pinger
 ```
 
-## Usage:
+## Usage
 
 ```js
 const pinger = require("starblast-pinger");
 
-// Get system info without getting player names
-pinger.getSystemInfo("https://starblast.io/#1234").then(console.log);
-
-// Get system info with player names.
-pinger.getSystemInfo("https://starblast.io/#1234", {
-    players: false, // Whether or not to fetch player names
-    playersTimeout: 250, // Timeout for fetching player names
-    timeout: 5000 // Timeout for fetching system info in general
-}).then(console.log);
-// Do note that fetching player names is typically a way more expensive action.
-// If fetching names from multiple game systems, it is recommended to do so asynchronously.
+// Get system info
+pinger.getSystemInfo("https://starblast.io/#1234", { options }).then(console.log).catch(console.error);
 ```
-If the ping fails, it should return an empty object `{}`.
+Options to pass into the `options` object:
+
+| Option | Type |Description | Default value<br>(if null/undefined or omitted) |
+| - | - | - | - |
+| players | boolean | Whether or not to fetch player names<br>Please note that fetching player names is typically a way more expensive action. | true |
+| timeout | positive integer | Timeout for fetching system info in general | 5000 |
+| playersTimeout | positive integer | Timeout for fetching player names, counting after system info is successfully fetched | 3000 |
+| preferredRegion | string | Preferred region in case there are multiple games in different regions with the same ID | none |
+| maxGetID | positive integer | To declare the highest player ID to get | 3x the fetching game's capacity (if found) |
+
+For example:
+```js
+pinger.getSystemInfo('https://starblast.io/#5349', {
+    players: true,
+    playersTimeout: 3000,
+    timeout: 5000,
+    preferredRegion: "America",
+    maxGetID: null
+}).then(console.log).catch(console.error);
+```
+
+If fetching names from multiple game systems, it is recommended to do so asynchronously.
+
+If the ping fails by any reason, the promise rejects with a specific error.
 
 If successful, it should return something akin to this (players object is only present if the players parameter is set to `true`):
 
-```
+```js
 {
   version: 84,
   seed: 6373,
@@ -86,17 +102,15 @@ If successful, it should return something akin to this (players object is only p
     teams: [ [Object], [Object], [Object] ]
   },
   region: 'America',
-  players: {
-    '1': { id: 1, hue: 96, player_name: 'NYOTA UHURA', custom: null },
-    '3': { id: 3, hue: 324, player_name: 'ARKADY DARELL', custom: null },
-    '4': { id: 4, hue: 48, player_name: 'EXAMPLE PLAYER', custom: null },
-    '5': { id: 5, hue: 252, player_name: 'ANOTHER PLAYER', custom: [Object] },
-    '6': { id: 6, hue: 12, player_name: 'EBLING MIS', custom: [Object] }
-  }
-
+  players: [
+    { id: 1, hue: 96, player_name: 'NYOTA UHURA', custom: null },
+    { id: 3, hue: 324, player_name: 'ARKADY DARELL', custom: null },
+    { id: 4, hue: 48, player_name: 'EXAMPLE PLAYER', custom: null },
+    { id: 5, hue: 252, player_name: 'ANOTHER PLAYER', custom: [Object] },
+    { id: 6, hue: 12, player_name: 'EBLING MIS', custom: [Object] }
+  ]
 }
 ```
 
-If you're not interested in any detailed info, a method `.systemExists` takes in a system URL and returns a `Promise<Boolean>`
-which resolves to `true` is a system exists and can be joined, and `false` if a system doesn't exist or
-could not be joined.
+If you're not interested in any detailed info, a method `.canPing` takes in a system URL and returns a `Promise<Boolean>`
+which resolves to `true` if it can join a system, and `false` if it fails (game is full or doesn't exist, connection unexpectedly closed, etc.)
--- index.js
+++ index.js
@@ -6,18 +6,23 @@ const axios = require("axios");
 
 // Returns simstatus.json
 const getSimStatus = async function () {
+  try {
     let response = await axios.get(`https://starblast.io/simstatus.json?cachebypass=${Math.random()}`);
     return response.data || [];
+  }
+  catch (e) {
+    throw new Error('Failed to fetch the serverlist file')
+  }
 }
 
 // converts link into game and server address
 const parseLink = function (link) {
-  let match = String(link).match(/^[\n\t\r\s]*(\w+\:\/\/){0,1}(.+\@){0,1}starblast\.io(\:443){0,1}\/*(beta\/){0,1}((index|app|mobile)\.html){0,1}(\?[^\t\r\n\s\#\/\\]*)*\#(\d+)\@(\w.+)\:(\d+)[^\s\t\n\r]*[\n\t\r\s]*$/);
+  let match = String(link).match(/^[\n\t\r\s]*(\w+\:\/\/){0,1}(.+\@){0,1}starblast\.io(\:443){0,1}\/*(beta\/){0,1}((index|app|mobile)\.html){0,1}(\?[^\t\r\n\s\#\/\\]*)*\#(\d+)(\@(\w.+)\:(\d+)){0,1}[^\s\t\n\r]*[\n\t\r\s]*$/);
   return {
     id: match?.[8] != null ? +match[8] : null,
     server: match?.[9] != null ? {
       ip: match[9],
-      port: match?.[10] != null ? +match[10] : null
+      port: match?.[11] != null ? +match[11] : null
     } : null
   }
 }
@@ -31,17 +36,15 @@ const getWebSocketAddress = async function (url = String(), preferredRegion) {
     if (address.id == null) {
         throw new TypeError("Invalid URL provided. Make sure system URL is in the form \"https://starblast.io/#1234\" or \"https://starblast.io/#1234@127.0.0.1:3000\"");
     }
-    let
     if (address.server == null) {
-        let id = Number(address);
+        let id = address.id;
         let simStatus = await getSimStatus();
         if (preferredRegion != null) simStatus.sort((sA, sB) => (sB.location === preferredRegion) - (sA.location === preferredRegion));
         Search: for (let location of simStatus) {
-            for (let systemIndex in location.systems) {
-                let system = location.systems[systemIndex];
+            for (let system of location.systems) {
                 if (system.id === id) {
                     let splitter = location.address.split(":");
-                    let {ip, port} = address.server = {
+                    address.server = {
                       ip: splitter[0],
                       port: +splitter[1]
                     }
@@ -51,28 +54,33 @@ const getWebSocketAddress = async function (url = String(), preferredRegion) {
         }
     }
     if (address.server == null) throw new Error("No public games found with given game ID. Please provide server address in the link if you want to ping a custom game");
-    address.server.ws = `wss://${ip.replace(/\./g, "-")}.starblast.io:${port}/`;
+    address.server.ws = `wss://${address.server.ip.replace(/\./g, "-")}.starblast.io:${address.server.port}/`;
     return address
 }
 
 // Gets system info given URL
-const getSystemInfo = function (url = String(), options = {preferredRegion: null, players: false, playersTimeout: 250, timeout: 5000}) {
+const getSystemInfo = function (url = String(), options = {preferredRegion: null, players: false, playersTimeout: 3000, timeout: 5000}) {
     try {
         return new Promise((resolve, reject) => {
             getWebSocketAddress(url, options.preferredRegion).then(info => {
-              address = info.server.ws;
-              let id = info.id;
-              options.timeout = options.timeout || 5000;
-              options.playersTimeout = options.playersTimeout || 250;
               try {
-                  let done = false;
-                  setTimeout(() => {
-                      socket.close();
-                      done = true;
-                      reject(new Error("Timed out"));
-                  }, options.timeout);
-                  let socket = new WebSocket(address, "echo-protocol", {origin: "https://starblast.io"});
-                  socket.onopen = function () {
+                  options.timeout = Math.trunc(Math.max(options.timeout, 1)) || 5000;
+                  options.playersTimeout = Math.trunc(Math.max(options.playersTimeout, 1)) || 3000;
+                  let output, error;
+                  let socket = new WebSocket(info.server.ws, "echo-protocol", {origin: "https://starblast.io"});
+                  let closeSocket = function (socket) {
+                    if (!socket.__CLOSED__) {
+                      socket.__CLOSED__ = true;
+                      socket.close()
+                    }
+                  }, setError = function (message) {
+                    if (error == null) {
+                      error = message;
+                      closeSocket(socket)
+                    }
+                  }
+                  let generalTimeout = setTimeout(setError, options.timeout, "Connection timed out"), playersTimeout;
+                  socket.on('open', function () {
                       socket.send(JSON.stringify({
                           "name": "join",
                           "data": {
@@ -80,7 +88,7 @@ const getSystemInfo = function (url = String(), options = {preferredRegion: null
                               "spectate_ship": 1,
                               "player_name": "starblast-pinger",
                               "hue": 240,
-                              "preferred": id,
+                              "preferred": info.id,
                               "bonus": "true",
                               "steamid": null,
                               "create": false,
@@ -88,17 +96,17 @@ const getSystemInfo = function (url = String(), options = {preferredRegion: null
                               "client_tr": 2.799774169921875
                           }
                       }));
-                  }
-                  let output;
-                  socket.onmessage = function (message) {
-                      let content = message.data;
-                      if (typeof content === "string" && content.startsWith("{")) {
-                          let data = JSON.parse(content);
+                  });
+                  socket.on('message', function (message, isBinary) {
+                      if (socket.__CLOSED__) return socket.close();
+                      if (!isBinary) try {
+                          let data = JSON.parse(message.toString());
                           if (data.name === "welcome") {
+                              clearInterval(generalTimeout);
                               output = data.data;
                               if (options.players) {
                                   output.players = [];
-                                  for (let x = 0; x <= output.mode.max_players * 3; x++) {
+                                  for (let x = 0; x <= (Math.trunc(Math.max(options.maxGetID, 0)) || output.mode.max_players * 3); ++x) {
                                       socket.send(JSON.stringify({
                                           "name": "get_name",
                                           "data": {
@@ -106,38 +114,34 @@ const getSystemInfo = function (url = String(), options = {preferredRegion: null
                                           }
                                       }));
                                   }
-                                  setTimeout(() => {
-                                      done = true;
-                                      socket.close();
-                                      resolve(output);
-                                  }, options.playersTimeout);
-                              } else {
-                                  done = true;
-                                  socket.close();
-                                  resolve(output);
+                                  playersTimeout = setTimeout(closeSocket, options.playersTimeout, socket);
                               }
-                          } else if (data.name === "cannot_join") {
-                              resolve({error: "cannot_join"});
-                          } else if (data.name === "player_name") {
-                              output.players[data.data.id] = data.data;
+                              else closeSocket(socket)
                           }
+                          else if (data.name === "cannot_join") setError("Could not join the requested game. Perhaps the game is full/empty or has expired?");
+                          else if (data.name === "player_name") output.players.push(data.data)
+                      } catch (e) {}
+                  });
+                  socket.on('close', function () {
+                      clearTimeout(generalTimeout);
+                      clearTimeout(playersTimeout);
+                      if (output) {
+                        delete output.mode.restore_ship;
+                        output.players?.sort?.((a, b) => a.id - b.id);
+                        resolve(output)
                       }
-                  }
-                  socket.onclose = function () {
-                      if (!done) reject(new Error("Connection closed unexpectedly"));
-                  }
-                  socket.onerror = function() {
-                      done = true;
-                      socket.close();
-                      reject(new Error("An error occured while establishing connection to the game"))
-                  }
+                      else reject(new Error(error ?? "Connection closed unexpectedly. Perhaps your IP has gotten rate-limited by the Starblast servers?"))
+                  });
+                  socket.on('error', function() {
+                      setError("An error occured while establishing connection to the game");
+                  })
               } catch (e) {
-                  reject(new Error("An unknown error occured. Please contact @dankdmitron#5029 in Discord if this error shows"))
+                  reject(new Error("An unknown error occured. Please contact @dankdmitron#5029 in Discord if this happens"))
               }
             }).catch(reject)
         });
     } catch (e) {
-        reject(new Error("An unknown error occured. Please contact @dankdmitron#5029 in Discord if this error shows"))
+        reject(new Error("An unknown error occured. Please contact @dankdmitron#5029 in Discord if this happens"))
     }
 }
 
